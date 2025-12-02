b46587de48b434279a50fde29fb87745f7e0a150
Thomas Durand <thomas.durand.verjat@gmail.com>
2025-12-03 00:14:16 +0100

    Solved Day 2

--- README.md
+++ README.md
@@ -25,6 +25,7 @@ Some special attention will be addressed regarding performances, I'll try to sol
 | Day    | Computing time\* | Part 1               | Part 2         |
 |--------|------------------|----------------------|----------------|
 | **1**  | 0,0012s          | 1074                 | 6254           |
-| **2**  | TBA              | TBA                  | TBA            |
+| **2**  | 0,0026s          | 29940924880          | 48631958998    |
+| **3**  | TBA              | TBA                  | TBA            |
 
 \* : Best elapsed time (after a few iterations) of the complete solution using "Release" mode on a MacBook Pro 14in 2021 with M1 Pro 10 CPU 16 GPU and 32GB of memory
--- Sources/Day02/Day02.swift
+++ Sources/Day02/Day02.swift
@@ -15,37 +15,118 @@ import Common
 
 @main
 struct Day02: Puzzle {
-    typealias Input = String
-    typealias OutputPartOne = Never
-    typealias OutputPartTwo = Never
+    typealias Input = [ProductIdRange]
+    typealias OutputPartOne = Int
+    typealias OutputPartTwo = Int
+
+    static var componentsSeparator: InputSeparator {
+        .string(string: ",")
+    }
 }
+
+struct ProductIdRange: Parsable, CustomStringConvertible {
+    let range: ClosedRange<Int>
+
+    static func parse(raw: String) throws -> Self {
+        let parts = raw.components(separatedBy: "-")
+        if parts.count != 2 {
+            throw InputError.unexpectedInput(unrecognized: raw)
+        }
+        guard let lowest = Int(parts[0]), let highest = Int(parts[1]) else {
+            throw InputError.unexpectedInput(unrecognized: raw)
+        }
+        return .init(range: lowest...highest)
+    }
+
+    var description: String {
+        return "\(range.lowerBound)-\(range.upperBound)"
+    }
+
+    func twoPartsInvalidIds() -> Set<Int> {
+        let lowest = range.lowerBound.description
+        let highest = range.upperBound.description
+        if lowest.count != highest.count {
+            // Split the range, and agglomerate
+            let limit = Int(pow(Double(10), Double(lowest.count)))
+            let first = ProductIdRange(range: range.lowerBound...limit-1).twoPartsInvalidIds()
+            let second = ProductIdRange(range: limit...range.upperBound).twoPartsInvalidIds()
+            return first.union(second)
+        }
+        if lowest.count == 1 || lowest.count % 2 == 1 {
+            // No invalid ID for asymetric stuff
+            return []
+        }
+        // Take half the lowest, half the highest
+        return invalidIdsOfParts(ofSize: lowest.count / 2)
+    }
+
+    func anyPartsInvalidIds() -> Set<Int> {
+        let lowest = range.lowerBound.description
+        let highest = range.upperBound.description
+        if lowest.count != highest.count {
+            // Split the range, and agglomerate
+            let limit = Int(pow(Double(10), Double(lowest.count)))
+            let first = ProductIdRange(range: range.lowerBound...limit-1).anyPartsInvalidIds()
+            let second = ProductIdRange(range: limit...range.upperBound).anyPartsInvalidIds()
+            return first.union(second)
+        }
+        if lowest.count == 1 {
+            return []
+        }
+        var invalidIds: Set<Int> = []
+        for size in 1...lowest.count/2 {
+            invalidIds = invalidIds.union(invalidIdsOfParts(ofSize: size))
+        }
+        return invalidIds
+    }
+
+    func invalidIdsOfParts(ofSize size: Int) -> Set<Int> {
+        let lowest = range.lowerBound.description
+        let highest = range.upperBound.description
+        precondition(lowest.count == highest.count)
+
+        var invalidIds: Set<Int> = []
+        let lowestPotential = Int("\(lowest[lowest.startIndex..<lowest.index(after: lowest.index(lowest.startIndex, offsetBy: size-1))])").unsafelyUnwrapped
+        let highestPotential = Int("\(highest[highest.startIndex..<highest.index(after: highest.index(highest.startIndex, offsetBy: size-1))])").unsafelyUnwrapped
+        for potential in lowestPotential...highestPotential {
+            let invalidId = Int(String(repeating: "\(potential)", count: lowest.count / size)).unsafelyUnwrapped
+            if range.contains(invalidId) {
+                invalidIds.insert(invalidId)
+            }
+        }
+        return invalidIds
+    }
+}
+
+let example = """
+11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124
+"""
 
 // MARK: - PART 1
 
 extension Day02 {
     static var partOneExpectations: [any Expectation<Input, OutputPartOne>] {
         [
-            // TODO: add expectations for part 1
+            assert(expectation: 1227775554, fromRaw: example)
         ]
     }
 
     static func solvePartOne(_ input: Input) async throws -> OutputPartOne {
-        // TODO: Solve part 1 :)
-        throw ExecutionError.notSolved
+        return input.reduce(Set<Int>(), { $0.union($1.twoPartsInvalidIds()) }).reduce(0, +)
     }
 }
 
 // MARK: - PART 2
+// 29940924880 too low
 
 extension Day02 {
     static var partTwoExpectations: [any Expectation<Input, OutputPartTwo>] {
         [
-            // TODO: add expectations for part 2
+            assert(expectation: 4174379265, fromRaw: example)
         ]
     }
 
     static func solvePartTwo(_ input: Input) async throws -> OutputPartTwo {
-        // TODO: Solve part 2 :)
-        throw ExecutionError.notSolved
+        return input.reduce(Set<Int>(), { $0.union($1.anyPartsInvalidIds()) }).reduce(0, +)
     }
 }
